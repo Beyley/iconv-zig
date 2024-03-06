22837629f82b5f67b619ee6e7102dfe506d0881b
Beyley Thomas <ep1cm1n10n123@gmail.com>
2024-03-06 12:26:39 -0800

    bb

--- build.zig
+++ build.zig
@@ -7,7 +7,7 @@ pub fn build(b: *std.Build) void {
     b.installArtifact(createIconv(b, target, optimize));
 }
 
-pub fn createIconv(b: *std.Build, target: std.zig.CrossTarget, optimize: std.builtin.OptimizeMode) *std.Build.CompileStep {
+pub fn createIconv(b: *std.Build, target: std.Build.ResolvedTarget, optimize: std.builtin.OptimizeMode) *std.Build.Step.Compile {
     const lib = b.addStaticLibrary(.{
         .name = "iconv-zig",
         .root_source_file = .{ .path = root_path ++ "src/main.zig" },
@@ -18,9 +18,9 @@ pub fn createIconv(b: *std.Build, target: std.zig.CrossTarget, optimize: std.bui
     lib.linkLibC();
 
     //On windows, lets use win-iconv, since windows does not provide an iconv implementation
-    if (target.getOsTag() == .windows) {
-        lib.addIncludePath(root_path ++ "win-iconv/");
-        lib.addCSourceFiles(&.{root_path ++ "win-iconv/win_iconv.c"}, &.{});
+    if (target.result.os.tag == .windows) {
+        lib.addIncludePath(.{ .path = root_path ++ "win-iconv/" });
+        lib.addCSourceFile(.{ .file = .{ .path = root_path ++ "win-iconv/win_iconv.c" }, .flags = &.{} });
     }
 
     return lib;
